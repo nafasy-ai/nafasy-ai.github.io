a3a566cf1ee19e71872de289145bf2d8ea320caa
ch-ayesha <ayesha.76e@gmail.com>
2025-08-13 16:51:32 +0500

    update website name

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Nafasay.AI',
+  title: 'Nafasy.AI',
   description: 'Created with Nafasa',
 }
 
