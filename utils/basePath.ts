5b0835c9c7c73e2a4e33ebdf62293639d9a306b6
ch-ayesha <ayesha.76e@gmail.com>
2025-08-15 11:54:41 +0500

    update usecase image

--- utils/basePath.ts
+++ utils/basePath.ts
@@ -3,3 +3,5 @@ const isGithubPages = process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'github';
 
 export const withBasePath = (path: string) =>
   isGithubPages ? `/nafasy-ai.github.io${path}` : path;
+
+
